baceee754e1914fbd4295dea944e0cf14666ed60
James Ho <jamesho19@gmail.com>
2020-11-01 00:01:48 +0800

    Add Card, Listtile Widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,51 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/howine.jpg'),
-            ),
-            Text(
-              'Howine Cafe',
-              style: TextStyle(fontSize: 20, fontFamily: 'Pacifico'),
-            ),
-            Text('Flutter Project',
-                style: TextStyle(fontSize: 20, fontFamily: 'SourceSansPro'))
-          ],
+            child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/howine.jpg'),
+              ),
+              Text(
+                'Howine Cafe',
+                style: TextStyle(fontSize: 20, fontFamily: 'Pacifico'),
+              ),
+              Text('Flutter Project',
+                  style: TextStyle(fontSize: 20, fontFamily: 'SourceSansPro')),
+              SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(color: Colors.teal.shade100)),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 30.0,
+                    color: Colors.teal,
+                  ),
+                  title: Text('user@gmail.com'),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.account_box_outlined,
+                    size: 30.0,
+                    color: Colors.teal,
+                  ),
+                  title: Text('password'),
+                ),
+              ),
+            ],
+          ),
         )),
       ),
     );
